1aa7027f71d6545b4de33cc18191e91291482981
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2021-06-18 14:08:28 -0300

    rentals implemented

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import cors from "cors"
 import express from "express"
 import pg from "pg"
 import joi from "joi"
+import dayjs from "dayjs"
 
 const {Pool} = pg
 
@@ -108,9 +109,17 @@ const gamesSchema = joi.object({
 
 //LISTAR CLIENTES TODOS
 app.get("/customers", async (req,res)=>{
-    const result= await connection.query('SELECT * FROM customers')
-    console.log(result.rows)
-    res.send(result.rows)
+    try{
+        if(req.query.cpf){
+            const result= await connection.query('SELECT * FROM customers WHERE cpf ILIKE $1',[req.query.cpf+'%'])
+            res.send(result.rows)
+        }else{
+            const result= await connection.query('SELECT * FROM customers')
+            res.send(result.rows)
+        }
+    }catch(e){
+        console.log(e)
+    }
 })
 
 //LISTAR CLIENTES UNICO
@@ -165,16 +174,112 @@ app.put("/customers/:id", async (req,res)=>{
     }
 })
 
-const customerIdSchema = joi.object({
-    id: joi.number().integer().min(1).required()
-})
-
 const customersSchema = joi.object({
     name: joi.string().min(1).required().trim(),
     phone: joi.string().required().trim().pattern(/^[0-9]{10,11}$/),
     cpf: joi.string().required().trim().pattern(/^[0-9]{11}$/),
     birthday: joi.string().required().trim(),//.pattern(/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/),
 })
+
+
+//LISTAR ALUGUEIS
+app.get("/rentals", async (req,res)=>{
+    // const rentals= await connection.query(`SELECT rentals.*, customers.* AS "rentals.customers", games.* AS "rentals.games"
+    //     FROM rentals FULL JOIN customers ON rentals."customerId" = customers.id 
+    //     FULL JOIN games ON games.id = rentals."gameId"`)
+    //     console.log(rentals)
+    //     res.send(rentals.rows)
+    
+    try{
+        const rentals= await connection.query('SELECT * FROM rentals')
+        const games= await connection.query('SELECT * FROM games')
+        const customers= await connection.query('SELECT * FROM customers')
+        const categories= await connection.query('SELECT * FROM categories')
+        rentals.rows.forEach(rental=>{
+            const game= games.rows.find(g=>g.id===rental.gameId)
+            const customer= customers.rows.find(c=>c.id===rental.customerId)
+            const category= categories.rows.find(c=>c.id===game.categoryId)
+            rental.customer={}
+            rental.customer.id= customer.id
+            rental.customer.name= customer.name
+            rental.game= {}
+            rental.game.id= game.id
+            rental.game.name= game.name
+            rental.game.categoryId= game.categoryId
+            rental.game.categoryName= category.name
+        })
+        if(req.query.customerId && req.query.gameId){
+            console.log("primeiro")
+            const result = rentals.rows.filter(rental=> rental.customerId===parseInt(req.query.customerId) && rental.gameId===parseInt(req.query.gameId))
+            console.log(result)
+            res.send(result)
+            return
+        }
+        if(req.query.customerId){
+            console.log("segundo")
+            const result = rentals.rows.filter(rental=>rental.customerId===parseInt(req.query.customerId))
+            console.log(result)
+            res.send(result)
+            return
+        }
+        if(req.query.gameId){
+            console.log("terceiro")
+            console.log(req.query.gameId)
+            console.log(parseInt(req.query.gameId))
+            const result= rentals.rows.filter(rental=>rental.gameId===parseInt(req.query.gameId))
+            console.log(result)
+            res.send(result)
+            return
+        }
+        console.log(rentals.rows)
+        res.send(rentals.rows)
+    }catch(e){
+        console.log(e)
+    }
+})
+
+//ADICIONAR ALUGUEL
+app.post("/rentals", async (req,res)=>{
+    console.log(dayjs(Date.now()).format("YYYY-MM-DD"))
+    const validatedRental = rentalSchema.validate(req.body)
+    if(validatedRental.error){
+        res.sendStatus(400)
+        return
+    }
+    const newRental=validatedRental.value;
+    try{
+        const customerExists= await connection.query('SELECT * FROM customers WHERE id = $1',[newRental.customerId])
+        if(!customerExists.rows.length){
+            res.sendStatus(400)
+            return
+        }
+        const gameExists= await connection.query('SELECT * FROM games WHERE id = $1',[newRental.gameId])
+        if(!gameExists.rows.length){
+            res.sendStatus(400)
+            return
+        }
+        const gameAvaliable= await connection.query('SELECT * FROM rentals WHERE "gameId" = $1',[newRental.gameId])
+        if(gameAvaliable.rows.length===gameExists.rows[0].stockTotal){
+            res.sendStatus(400)
+            return
+        }
+        const result= await connection.query(`
+            INSERT INTO rentals 
+            ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") 
+            VALUES ($1,$2,$3,$4,$5,$6,$7)
+        `,[newRental.customerId,newRental.gameId,dayjs(Date.now()).format("YYYY-MM-DD"),newRental.daysRented, null,newRental.daysRented*gameExists.rows[0].pricePerDay,null ])
+        res.send(result)
+    }catch(e){
+        console.log(e)
+    }
+})
+
+const rentalSchema = joi.object({
+    customerId: joi.number().integer().min(1).required(),
+    gameId: joi.number().integer().min(1).required(),
+    daysRented: joi.number().integer().min(1).required()
+})
+
 
 app.listen(4000, ()=>{
     console.log("Server running on port 4000") 
